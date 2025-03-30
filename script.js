204e3b0029fd75fe8b18aede87217580a3f51951
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-03-30 15:10:45 +0200

    JS modfy second hand to move continuously

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const hr = document.querySelector('.clock .hour-container .hour');
 const mn = document.querySelector('.clock .minute-container .minute');
 const sc = document.querySelector('.clock .second-container .second');
 
-setInterval(()=>{
+const day = new Date();
 
-    const day = new Date();
+const s = day.getSeconds();
+let ms = s *1000;
+
+setInterval(()=>{    
 
     const h = day.getHours() >= 12 ? day.getHours() -12 : day.getMinutes();
-    // const h = 5;
     const m = day.getMinutes();
-    const s = day.getSeconds();
-
+    ms += 10 ;
 
     hr.style.transform = `rotate(${((360 / 12) * h) + (360 / (12 * 60) * m)}deg)`; 
     mn.style.transform = `rotate(${(360 / 60) * m}deg)`; 
-    sc.style.transform = `rotate(${(360 / 60) * s}deg)`; 
+    sc.style.transform = `rotate(${360 / (60 * 1000) * ms}deg)`;    
     
-    console.log(h, m , s);
-}, 1000);
+}, 10);
